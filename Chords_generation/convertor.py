c612d90a30e911d95f8f3ec8d943a386c2ba3738
Lucie263 <62712767+Lucie263@users.noreply.github.com>
2023-10-20 20:09:32 +0200

    convertor réparer

--- Chords_generation/convertor.py
+++ Chords_generation/convertor.py
@@ -1,39 +1,28 @@
-""" 
-This scripts permits the convertion of midi files into wav files
-To use it first start by "pip install mingus" on your env
-"""
-
 import subprocess
 from os import walk
 
-fluidsynth_path = r'midi_utils\fluidsynth-2.3.4-win10-x64\bin\fluidsynth.exe'
+fluidsynth_path = r"Chords_generation\\midi_utils\\fluidsynth-2.3.4-win10-x64\\bin\\fluidsynth.exe"
 
-# Uncomment the font you like, in the created files will be recognized by a letter, G for Guitar, P for Piano, F for flute and V for Violin
-
-soundfont_file = 'midi_utils\Guitar.SF2' 
+soundfont_file = r"Chords_generation\\midi_utils\\Guitar.SF2" 
 instrument = 'G_'
 
-# soundfont_file = 'midi_utils\Flute.sf2'
-# instrument = 'F_'
-
-# soundfont_file = 'midi_utils\Piano.sf2'
-# instrument = 'P_'
-
-# soundfont_file = 'midi_utils\RolandMarcatoStrings.sf2'
-# instrument = 'V_'
-
-
 listeFichiers = []
-for (repertoire, sousRepertoires, fichiers) in walk('chords_midi'):
+for (repertoire, sousRepertoires, fichiers) in walk("Chords_generation\\chords_midi"):
  listeFichiers.extend(fichiers)
 
+print(listeFichiers)
+
 for file in listeFichiers:
-   midi_file = "chords_midi/" + file
-   output_wav = "chords_wav/" + instrument + file.split('.')[0] + ".wav"
+   midi_file = "Chords_generation\\chords_midi\\" + file
+   output_wav = "Chords_generation\\chords_wav\\" + instrument + file.split('.')[0] + ".wav"
+   print(midi_file)
    command = [fluidsynth_path, '-a', 'dsound', '-o', 'audio.driver=dsound', '-i', '-l', '-s', '-g', '1.0', soundfont_file, midi_file, '-F', output_wav]
 
    try:
     subprocess.run(command, check=True, shell=True)
     print(f'OK ==> {output_wav}')
    except subprocess.CalledProcessError as e:
-    print(f'ERROR : {e}')    
+    print(f'ERROR : {e}') 
+
+   
+
