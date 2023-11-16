643b5108fea39a876b4be43dc6f5656fe92edf2c
Lucie263 <62712767+Lucie263@users.noreply.github.com>
2023-11-16 13:40:55 +0100

    RN le plus proche de l'article

--- Chords_generation/convertor.py
+++ Chords_generation/convertor.py
@@ -6,6 +6,9 @@ fluidsynth_path = r"Chords_generation\\midi_utils\\fluidsynth-2.3.4-win10-x64\\b
 soundfont_file = r"Chords_generation\\midi_utils\\Guitar.SF2" 
 instrument = 'G_'
 
+# soundfont_file = r"Chords_generation\\midi_utils\\Guitar_StudioPack.SF2" 
+# instrument = 'G_'
+
 # soundfont_file = r"Chords_generation\midi_utils\Flute.sf2" 
 # instrument = 'F_'
 
@@ -21,10 +24,10 @@ for (repertoire, sousRepertoires, fichiers) in walk("Chords_generation\\chords_m
 
 print(listeFichiers)
 
-for i in range(200):
+for i in range(100):
    for file in listeFichiers:
       midi_file = "Chords_generation\\chords_midi\\" + file
-      output_wav = "Chords_generation\\chords_wav\\" + instrument + file.split('.')[0]+'_'+ str(i) + ".wav"
+      output_wav = "Chords_generation\\chords_wav\\" + instrument + file.split('.')[0]+'_'+ str(i)#+100) + ".wav"
       print(midi_file)
       command = [fluidsynth_path, '-a', 'dsound', '-o', 'audio.driver=dsound', '-i', '-l', '-s', '-g', '1.0', soundfont_file, midi_file, '-F', output_wav]
 
