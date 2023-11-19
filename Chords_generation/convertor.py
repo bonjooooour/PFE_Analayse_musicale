6ffacc9ae4d67e37c52f368285b9cdb58abed9d0
Lucie263 <62712767+Lucie263@users.noreply.github.com>
2023-11-19 19:30:14 +0100

    RN for clean, noisy and both guitares

--- Chords_generation/convertor.py
+++ Chords_generation/convertor.py
@@ -3,16 +3,16 @@ from os import walk
 
 fluidsynth_path = r"Chords_generation\\midi_utils\\fluidsynth-2.3.4-win10-x64\\bin\\fluidsynth.exe"
 
-soundfont_file = r"Chords_generation\\midi_utils\\Guitar.SF2" 
-instrument = 'G_'
-
-# soundfont_file = r"Chords_generation\\midi_utils\\Guitar_StudioPack.SF2" 
+# soundfont_file = r"Chords_generation\\midi_utils\\Guitar.SF2" 
 # instrument = 'G_'
+
+soundfont_file = r"Chords_generation\\midi_utils\\Guitar_StudioPack.SF2" 
+instrument = 'G2_'
 
 # soundfont_file = r"Chords_generation\midi_utils\Flute.sf2" 
 # instrument = 'F_'
 
-# soundfont_file = r"Chords_generation\midi_utils\Piano.sf2" 
+# soundfont_file = r"Chords_generation\midi_utils\198_VL_1_wurlitzer_VS.sf2" 
 # instrument = 'P_'
 
 # soundfont_file = r"Chords_generation\midi_utils\198_Legato_strings.sf2" 
@@ -27,7 +27,7 @@ print(listeFichiers)
 for i in range(100):
    for file in listeFichiers:
       midi_file = "Chords_generation\\chords_midi\\" + file
-      output_wav = "Chords_generation\\chords_wav\\" + instrument + file.split('.')[0]+'_'+ str(i)#+100) + ".wav"
+      output_wav = "Chords_generation\\chords_wav\\" + instrument + file.split('.')[0]+'_'+ str(i) + ".wav"
       print(midi_file)
       command = [fluidsynth_path, '-a', 'dsound', '-o', 'audio.driver=dsound', '-i', '-l', '-s', '-g', '1.0', soundfont_file, midi_file, '-F', output_wav]
 
